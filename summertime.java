c608e9b657f3b082ea35e1d6934fbd7457df7669
munawar900munawar <shaikskarishma9@gmail.com>
2024-12-09 09:54:11 +0530

    Add files via upload

---
+++ summertime.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class summertime
+{
+	public static void main (String[] args)
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int X=sc.nextInt();
+	 if(X<=35){
+	     System.out.println("NO");
+	 }else{
+	     System.out.println("YES");
+	 } 
+	}
+}
