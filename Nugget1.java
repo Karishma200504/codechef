3bd4ba85bb4aeee0cae42ac0f55138b6b1bc8bce
Karishma200504 <shaikskarishma9@gmail.com>
2024-12-10 15:09:36 +0530

    Add files via upload

---
+++ Nugget1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Nugget1
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int z=x*5000+y*9800;
+		System.out.println(z);
+	}
+}
