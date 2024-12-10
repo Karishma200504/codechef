5ce21b30c28004558c81e0e8facedc2c0d98c24d
Karishma200504 <shaikskarishma9@gmail.com>
2024-12-10 15:20:20 +0530

    Add files via upload

---
+++ Foodcost.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Foodcost
+{
+	public static void main (String[] arg)
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int z=6*x+y;
+		System.out.println(z);
+
+	}
+}
