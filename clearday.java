69eb7e353f4e24a9f516abdda62e972f4d0c8355
Karishma200504 <shaikskarishma9@gmail.com>
2024-12-15 11:00:42 +0530

    Add files via upload

---
+++ clearday.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class clearday
+{
+	public static void main (String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	int X=sc.nextInt();
+	int Y=sc.nextInt();
+	int a=7-(X+Y);
+	System.out.println(a);
+	}
+}
