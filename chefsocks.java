10ce9155d61a0cb03c02e04ea5ca1f9c397cfb49
Karishma200504 <shaikskarishma9@gmail.com>
2024-12-15 11:29:58 +0530

    Add files via upload

---
+++ chefsocks.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class chefsocks
+{
+	public static void main (String[] args)
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int A=sc.nextInt();
+	 int X=sc.nextInt();
+	 int Y=sc.nextInt();
+	 if(X+Y>A)
+	 System.out.println("Yes");
+	 else
+     System.out.println("NO");
+	}
+}
