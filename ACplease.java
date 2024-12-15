01c6c2b6ce38f3c663937c194b4802f4228277c6
Karishma200504 <shaikskarishma9@gmail.com>
2024-12-15 11:12:01 +0530

    Add files via upload

---
+++ ACplease.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ACplease
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		if(T<=30)
+		System.out.println("NO");
+		else
+		System.out.println("YES");
+	}
+}
