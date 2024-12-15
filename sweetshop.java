144b342a5dc3ac6956e8cc5fb8c3632a33e981d2
Karishma200504 <shaikskarishma9@gmail.com>
2024-12-15 11:01:00 +0530

    Add files via upload

---
+++ sweetshop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class sweetshop
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int N=sc.nextInt();
+		int c=N*10;
+		System.out.println((X-c)/20);
+
+	}
+}
