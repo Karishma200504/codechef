af410d80f7945c1d0a70febc5a07674782d77a5c
Karishma200504 <shaikskarishma9@gmail.com>
2024-12-16 10:33:58 +0530

    Add files via upload

---
+++ luckyclover.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class luckyclover
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		if(N!=1)
+		System.out.println(4+(N-1)*3);
+		else
+		System.out.println(1*4);
+	}
+}
